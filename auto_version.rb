f7616e4274daeaee4333043197d1c3a17b925672
Rahul Kumar <rahulsinner@gmail.com>
2011-08-27 12:15:05 +0530

    Modifying to use timestamp inplace of hash.

--- auto_version.rb
+++ auto_version.rb
@@ -1,53 +1,44 @@
-require 'digest/md5'
-
 class App < Sinatra::Base
   helpers do
     include Rack::Utils
     alias_method :h, :escape_html
 
-# Auto-version static assets based on md5 or explicity supplied version.
-# Auto-versioned assets can have cache headers set to large durations.
-def auto_version(resource_id, use_hash=true, version=1, settings)
-  public_dir = settings['public'] || "public"
-  views_dir = settings['views']
-  dev_js_dir = File.join views_dir, "coffee"
-  dev_css_dir = File.join views_dir, "sass"
-  dir_name, file_name = File.dirname(resource_id), File.basename(resource_id)
+    # Auto-version static assets based on timestamp or explicity supplied version.
+    # Auto-versioned assets can have cache headers set to large durations.
+    def auto_version(resource_path, version=nil)
+      public_dir = settings.public || "public"
 
-  if /(?<name>\w+)(?<ext>.\w+)/ =~ file_name
-    # Hash not requested. Return explicitly versioned name.
-    if not use_hash
-      return File.join dir_name, "#{name}.#{version}#{ext}"
-    else
-      file_path = File.join public_dir, resource_id
-      # Change file paths to point to coffee source for js and sass source for
-      # css if running under development.
+      # Auto-versioning not requested. Return explicitly versioned name.
+      if version 
+        return "#{resource_path};#{version}"
+      else
+        file_path = File.join public_dir, resource_path 
+        file_path = translate_file_path(file_path, settings)
+        return "#{resource_path};#{File.mtime(file_path).to_i}"
+      end
+    end
+
+    # Changes file paths to point to coffee source for js and sass source for
+    # css if running under development.
+    def translate_file_path(file_path, settings)
+      views_dir = settings.views
+      dev_js_dir = File.join views_dir, "coffee"
+      dev_css_dir = File.join views_dir, "sass"
       env = settings.environment
+
       if env != "production"
         new_file_path = nil
-        if ext == ".js"
-          coffee_resource = file_name.sub(/\.js$/, '.coffee')
+        if file_path.end_with? "js"
+          coffee_resource = File.basename(file_path).sub(/\.js$/, '.coffee')
           new_file_path = File.join dev_js_dir, coffee_resource
-        elsif ext == ".css"
-          sass_resource = file_name.sub(/\.css$/, '.sass')
+        elsif file_path.end_with? "css"
+          sass_resource = File.basename(file_path).sub(/\.css$/, '.sass')
           new_file_path = File.join dev_css_dir, sass_resource
         end
-        if new_file_path
-          file_path = new_file_path if File.exist? new_file_path
-        end
+        file_path = new_file_path if new_file_path and File.exist? new_file_path
       end
-      md5 = "" 
-      begin
-        md5 = Digest::MD5.hexdigest IO.read(file_path)
-      rescue IOError => ex
-        puts ex
-        return resource_id
-      end
-      return File.join dir_name, "#{name}.#{md5}#{ext}"
+      file_path
     end
-  else
-    return resource_id
-  end
-end
+
   end
 end
